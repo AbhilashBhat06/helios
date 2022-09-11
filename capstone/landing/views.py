8e5c0bce9837080b3b1159ea081424523758d0d6
Abhilash Bhat <06abhilashbhat@gmail.com>
2022-09-11 16:13:20 +0530

    Update landing/views.py

--- capstone/landing/views.py
+++ capstone/landing/views.py
@@ -3,9 +3,8 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 # Create your views here.
 
-class homePage(TemplateView):
-	template_name = 'index.html'
+# class homePage(TemplateView):
+# 	template_name = 'index.html'
 
-# def homePage(req):
-	# return render(req, 'index.html')
-	# return HttpResponse('Hello World!!')
+def homePage(req):
+	return render(req, 'index.html')
