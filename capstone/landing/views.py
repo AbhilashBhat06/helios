1fdcae37c54f053247dc12ddc25aa51a34df9be0
Abhilash Bhat <06abhilashbhat@gmail.com>
2022-09-11 18:30:43 +0530

    Add appropriate changes

--- capstone/landing/views.py
+++ capstone/landing/views.py
@@ -3,8 +3,28 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 # Create your views here.
 
-# class homePage(TemplateView):
-# 	template_name = 'index.html'
+"""
+class homePage(TemplateView):
+	template_name = 'index.html'
+
+class aboutPage(TemplateView):
+	template_name = 'about-us.html'
+
+class contactPage(TemplateView):
+	template_name = 'contact.html'
+
+class techPage(TemplateView):
+	template_name = 'tech.html'
+"""
 
 def homePage(req):
 	return render(req, 'index.html')
+
+def aboutPage(req):
+	return render(req, 'about-us.html')
+
+def contactPage(req):
+	return render(req, 'contact.html')
+
+def techPage(req):
+	return render(req, 'tech.html')
