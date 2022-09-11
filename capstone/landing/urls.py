db4dd0eaa7c8b27df2be28ff0c6b5eb8146d899b
Abhilash Bhat <06abhilashbhat@gmail.com>
2022-09-11 18:30:30 +0530

    Add appropriate changes

--- capstone/landing/urls.py
+++ capstone/landing/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
-from .views import homePage
+from .views import *
 
 urlpatterns = [
-	# path('', homePage.as_view()),.
-	path('', homePage)
+	# path('', homePage.as_view()),
+	# path('about', aboutPage.as_view()),
+	# path('contact', contactPage.as_view()),
+	# path('tech', techPage.as_view())
+	path('', homePage),
+	path('about/', aboutPage),
+	path('contact/', contactPage),
+	path('tech/', techPage)
 ]
