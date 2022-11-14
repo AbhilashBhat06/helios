fd0e3e339fef4330d24f56d99b75ba4f91d53198
Abhilash Bhat <06abhilashbhat@gmail.com>
2022-11-14 15:25:18 +0530

    Setup django models for API

--- capstone/landing/models.py
+++ capstone/landing/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class buildings(models.Model):
+    LOCATION_CHOICES = (
+        ("Electronic City", "Electronic City"),
+        ("Whitefield", "Whitefield"),
+        ("Jayanagar", "Jayanagar"),
+        ("Marathalli", "Marathalli"),
+    )
+    DATES_CHOICES = (
+        ("12-11-22", "12-11-22"),
+        ("13-11-22", "13-11-22"),
+        ("14-11-22", "14-11-22"),
+    )
+
+    SUBJECT_CHOICES = (
+        ("Government", "Government"),
+        ("Corporate", "Corporate"),
+        ("Hobbyist", "Hobbyist"),
+        ("Student", "Student"),
+    )
+
+    location = models.CharField(
+        max_length=50, default="Electronic City", choices=LOCATION_CHOICES
+    )
+    date = models.DateTimeField(default="14-11-22", choices=DATES_CHOICES)
+    subject = models.CharField(
+        max_length=25, default="Government", choices=SUBJECT_CHOICES
+    )
+    # image = models.ImageField(
+    # upload_to=None, height_field=100, width_field=100, max_length=100
+    # )
